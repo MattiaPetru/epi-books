11e807cee88d240999b109e6e41b19eedc392f31
MattiaPetru <mattiapetru93@gmail.com>
2024-06-13 19:39:41 +0200

    modifiche

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
-import { Button, Container } from 'react-bootstrap';
+import { Container } from 'react-bootstrap';
 import './App.css';
 import MyFooter from './components/MyFooter';
 import MyNav from './components/MyNav';
-import Welcome from './components/Welcome';
-import AllTheBooks from './components/AllTheBooks';
 import fantasy from "./books/fantasy.json"
 import history from "./books/history.json"
 import horror from "./books/horror.json"
